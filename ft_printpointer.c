6a8b85e7746729c5ba4a3b61401b49eb901f0baa
SarkaKv <145465445+SarkaKv@users.noreply.github.com>
2024-06-24 12:09:41 +0200

    Update ft_printpointer.c

--- ft_printpointer.c
+++ ft_printpointer.c
@@ -4,8 +4,8 @@ int ft_printpointer(void *ptr)
     unsigned long adress = (unsigned long)oki;
     int lenght = 2;
 
-    ft_printchar(0);
-    ft_printchar(x);
+    ft_printchar('0');
+    ft_printchar('x');
     lenght += ft_printhexadecimallower(adress);
     return(lenght);
 }

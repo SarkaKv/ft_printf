2854101df56328b200f7705e6987d0e70e15c653
SarkaKv <145465445+SarkaKv@users.noreply.github.com>
2024-06-24 10:33:12 +0200

    Create ft_printhexadecimalupper.c

---
+++ ft_printhexadecimalupper.c
@@ -0,0 +1,36 @@
+#include "libftprintf.h"
+
+static int ft_hexlenght(int nuber)
+{
+    int lenght = 0;
+    if(nuber == 0)
+        return 1;
+    while(nuber != 0)
+    {
+        nuber = nuber/16;
+        lenght++;
+    }
+    return(lenght);
+}
+
+static int ft_puthex(int nuber)
+{
+
+    if(nuber >= 0 && nuber <= 9)
+        ft_printchar(nuber + '0');
+    else if(nuber >= 10 && nuber <= 15)
+        ft_printchar(nuber - 10 + 'A');
+
+    else
+    {
+    ft_puthex(nuber/16);
+    ft_puthex(nuber%16);
+    }
+}
+
+int ft_printhexadecimalupper(int number)
+{
+    int lenght = ft_hexlenght(number);
+    ft_puthex(number);
+    return(lenght);
+}

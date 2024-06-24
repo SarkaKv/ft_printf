0d4175d8b2ff4f45661b7ed443587e9c2eaff4d4
SarkaKv <145465445+SarkaKv@users.noreply.github.com>
2024-06-24 10:34:26 +0200

    Create ft_printunsignedint.c

---
+++ ft_printunsignedint.c
@@ -0,0 +1,65 @@
+#include "libftprintf.h"
+
+int ft_printunsignedint(unsigned int number)
+{
+    char *ptr = my_second_itoa(number);
+    int mylenght = ft_strlen(ptr);
+    ft_printstring(ptr);
+    free(ptr);
+    return(mylenght);
+}
+static int ft_printstring(char *str)
+{
+	if(!str)
+		return(0);
+	int counting = 0;
+	while(str[counting] != '\0')
+	{
+		write(1, &str[counting], 1);
+		counting ++;
+	}
+	return(counting);
+}
+static int	getlenghtofn(unsigned int ok)
+{
+	unsigned int	lenghtofthenb;
+
+	lenghtofthenb = 0;
+	if (ok <= 0)
+	{
+		lenghtofthenb++;
+	}
+	while (ok != 0)
+	{
+		ok = ok / 10;
+		lenghtofthenb++;
+	}
+	return (lenghtofthenb);
+}
+
+static char	*my_second_itoa(unsigned int n)
+{
+	char			*thenumber;
+	size_t			lenghtofthenb;
+	unsigned int	tempn;
+
+	lenghtofthenb = getlenghtofn(n);
+	tempn = (unsigned int)n;
+	thenumber = (char *)malloc(sizeof(char) * (lenghtofthenb + 1));
+	if (!thenumber)
+		return (NULL);
+	thenumber[lenghtofthenb] = '\0';
+	if (n < 0)
+	{
+		thenumber[0] = '-';
+		tempn = -tempn;
+	}
+	if (n == 0)
+		thenumber[0] = '0';
+	while (tempn > 0)
+	{
+		thenumber[--lenghtofthenb] = (tempn % 10) + '0';
+		tempn = tempn / 10;
+	}
+	return (thenumber);
+}
